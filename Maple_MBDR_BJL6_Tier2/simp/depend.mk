2d81e13a01ba1cef4b0d2b5540fca7f33b1cb776
sima <sima@Babaks-MBP-2.lan>
2024-08-18 10:22:31 -0700

    Initial commit

---
+++ Maple_MBDR_BJL6_Tier2/simp/depend.mk
@@ -0,0 +1,14 @@
+/Users/sima/Documents/Permanent_Clauses_in_SAT_Solvers/Maple_MBDR_BJL6_Tier2/simp/Main.o /Users/sima/Documents/Permanent_Clauses_in_SAT_Solvers/Maple_MBDR_BJL6_Tier2/simp/Main.or /Users/sima/Documents/Permanent_Clauses_in_SAT_Solvers/Maple_MBDR_BJL6_Tier2/simp/Main.od /Users/sima/Documents/Permanent_Clauses_in_SAT_Solvers/Maple_MBDR_BJL6_Tier2/simp/Main.op: \
+  /Users/sima/Documents/Permanent_Clauses_in_SAT_Solvers/Maple_MBDR_BJL6_Tier2/simp/Main.cc \
+  ../utils/System.h ../mtl/IntTypes.h ../utils/ParseUtils.h \
+  ../utils/Options.h ../mtl/Vec.h ../mtl/XAlloc.h ../core/Dimacs.h \
+  ../core/SolverTypes.h ../mtl/Alg.h ../mtl/Map.h ../mtl/Alloc.h \
+  ../simp/SimpSolver.h ../mtl/Queue.h ../core/Solver.h ../mtl/Heap.h \
+  ../utils/ccnr.h
+/Users/sima/Documents/Permanent_Clauses_in_SAT_Solvers/Maple_MBDR_BJL6_Tier2/simp/SimpSolver.o /Users/sima/Documents/Permanent_Clauses_in_SAT_Solvers/Maple_MBDR_BJL6_Tier2/simp/SimpSolver.or /Users/sima/Documents/Permanent_Clauses_in_SAT_Solvers/Maple_MBDR_BJL6_Tier2/simp/SimpSolver.od /Users/sima/Documents/Permanent_Clauses_in_SAT_Solvers/Maple_MBDR_BJL6_Tier2/simp/SimpSolver.op: \
+  /Users/sima/Documents/Permanent_Clauses_in_SAT_Solvers/Maple_MBDR_BJL6_Tier2/simp/SimpSolver.cc \
+  ../mtl/Sort.h ../mtl/Vec.h ../mtl/IntTypes.h ../mtl/XAlloc.h \
+  ../simp/SimpSolver.h ../mtl/Queue.h ../core/Solver.h ../mtl/Heap.h \
+  ../mtl/Alg.h ../utils/Options.h ../utils/ParseUtils.h \
+  ../core/SolverTypes.h ../mtl/Map.h ../mtl/Alloc.h ../utils/ccnr.h \
+  ../utils/System.h
